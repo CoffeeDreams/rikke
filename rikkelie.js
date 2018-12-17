764badd858501ac7938e27d571d2b63313aa407f
CoffeeDreams <maja8769@stud.kea.dk>
2018-12-17 10:53:58 +0100

    commit_3

--- rikkelie.js
+++ rikkelie.js
@@ -1,11 +1,13 @@
+//https://www.majabay.dk/kea/wordpress_rikke/wp-admin/index.php
+
 //https://majabay.dk/kea/wordpress_rikke/wp-json/wp/v2/
 
 //https://majabay.dk/kea/rikkelie/
 
-
+const template_concert = document.querySelector("#concert-template")
 
 function getData(){
-    fetch("https://majabay.dk/kea/wordpress_rikke/wp-json/wp/v2/posts")
+    fetch("https://majabay.dk/kea/wordpress_rikke/wp-json/wp/v2/concerts")
     .then(response => response.json())
     .then(showPosts)
 }
